5309772f52a8ea1c4ed7d1df3e7bb1bfdb1cd239
JCav <micojcav@outlook.com>
2023-05-09 11:08:47 +0800

    yes, finished

---
+++ Screenoff_csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[DllImport("User32.dll")]
+static extern bool LockWorkStation();
+
+[DllImport("User32.dll")]
+static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+
+LockWorkStation();
+SendMessage(-1, 0x0112, 0xF170, 2);
+
+/**
+ * WM_SYSCOMMAND    0x0112
+ * SC_MONITORPOWER  0xF170
+ * lParam
+ *   * -1 is powering on
+ *   * 1  going to low power
+ *   * 2  shut off
+ */
